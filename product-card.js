5d87431ff5b7b0cb88b47aee8b1bec54f6245958
leonn00albert <42689994+leonn00albert@users.noreply.github.com>
2019-07-18 22:28:11 -0500

    Update product-card.js

--- product-card.js
+++ product-card.js
@@ -1,13 +1,30 @@
-productImage = "url('https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-2_large.png?v=1530129318')";
+// Object we’re working with
+var dataObject = {
+  0: {"productId":1,"productTitle":"product 1","productPrice":"$10,00","productVendor":"vendor 1","productImage":"https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-2_large.png?v=1530129318"},
+  1: {"productId":2,"productTitle":"product 2","productPrice":"$15,00","productVendor":"vendor 1","productImage":"https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_large.png?v=1530129360"},
+  2: {"productId":2,"productTitle":"product 3","productPrice":"$20,00","productVendor":"vendor 1","productImage":"https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-5_large.png?v=1530129458"}
 
-productTitle = 'Test Product';
-productPrice = '$10,00';
-productVendor = 'Test Vendor';
+};
 
-document.getElementById("product-image").style.backgroundImage = productImage;
-document.getElementById("product-title").innerHTML = productTitle;
-document.getElementById("product-price").innerHTML= productPrice;
-document.getElementById("product-vendor").innerHTML= productVendor;
+// Cache of the template
+var template = document.getElementById("template-list-item");
+// Get the contents of the template
+var templateHtml = template.innerHTML;
+// Final HTML variable as empty string
+var listHtml = "";
+
+// Loop through dataObject, replace placeholder tags
+// with actual data, and generate final HTML
+for (var key in dataObject) {
+  listHtml += templateHtml.replace(/{{productId}}/g, dataObject[key]["productId"])
+                          .replace(/{{productTitle}}/g, dataObject[key]["productTitle"])
+                          .replace(/{{productPrice}}/g, dataObject[key]["productPrice"])
+                          .replace(/{{productVendor}}/g, dataObject[key]["productVendor"])
+                          .replace(/{{productImage}}/g, dataObject[key]["productImage"]);
+}
+
+// Replace the HTML of #list with final HTML
+document.getElementById("list").innerHTML = listHtml;
 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
@@ -15,4 +32,6 @@ function openForm() {
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+
+
 }
